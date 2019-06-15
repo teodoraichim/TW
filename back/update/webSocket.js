3b9491068023c05dc13c7c91cbee26d05cbf5250
jjspielhurt <mariuta.silviu@yahoo.com>
2019-06-15 23:02:36 +0300

    Working updates+updated query

--- back/update/webSocket.js
+++ back/update/webSocket.js
@@ -62,18 +62,20 @@ io.on('connection', client => {
           let isAlready = false;
           if (clients[project_id])
             for (var i = 0; i < clients[project_id].length; i++) {
-              console.log(clients[project_id][i].user_id);
+              console.log("user_id:" + clients[project_id][i].user_id);
               if (clients[project_id][i].user_id == user_id)
                 isAlready = true;
-              if (!isAlready) {
-
-                if (clients[project_id]) clients[project_id].push({ "client": client, "user_id": user_id });
-                else {
-                  clients[project_id] = [{ "client": client, "user_id": user_id }]; 
-                }
-              }
             }
-          else { clients[project_id] = [{ "client": client, "user_id": user_id }];}
+          if (!isAlready) {
+
+            if (clients[project_id]) clients[project_id].push({ "client": client, "user_id": user_id });
+            else {
+              clients[project_id] = [{ "client": client, "user_id": user_id }];
+            }
+          }
+          else { clients[project_id].push({ "client": client, "user_id": user_id }); }
+
+
 
           console.log(clients);
 
@@ -111,7 +113,7 @@ io.on('connection', client => {
   });
 
   client.on('disconnect', () => {
-    
+
     Reflect.ownKeys(clients).forEach(project_id => {
       console.log(clients[project_id]);
       for (var i = 0; i < clients[project_id].length; i++) {
@@ -126,7 +128,7 @@ io.on('connection', client => {
     });
     console.log("Popped client");
     console.log(clients);
-    
+
   });
 });
 server.listen(3000);
